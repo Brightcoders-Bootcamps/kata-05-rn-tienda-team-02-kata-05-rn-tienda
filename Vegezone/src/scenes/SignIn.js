9fc8a70bb3febbe9ac2da0aa1fa5df6c26e0f415
vmagana3 <vmagana3@ucol.mx>
2021-02-09 13:02:29 -0600

    Text constants added and view completed

--- Vegezone/src/scenes/SignIn.js
+++ Vegezone/src/scenes/SignIn.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import {View,Text,TouchableOpacity} from 'react-native';
-import styles from '../styles/SignInSignUpStyles';
+import styles from '../styles/SignCreateStyles/SignInSignUpStyles';
 import InputText from '../components/atoms/InputText';
 import ButtonSignCreate from '../components/atoms/ButtonSignCreate';
+
+import {signInCreateConstants} from '../utils/Constants/SignCreateConstants'
+
+
 
 import emailIcon from '../assets/images/email.png'
 import passwordIcon from '../assets/images/password.png'
@@ -13,15 +17,15 @@ const SignIn = ()=>
         <View style={styles.mainContainer}>
 
             <View style={styles.mainContent}>
-                <Text style={styles.signinText}>Sign In</Text>   
-                <InputText placeholder={"Email"} imagePath={emailIcon}></InputText>  
-                <InputText secureEntryValue={true} placeholder={"Password"} imagePath={passwordIcon}></InputText> 
-                <Text style={styles.forgotText}>Forgot Password?</Text>  
-                <ButtonSignCreate ButtonText={"SIGN IN"}></ButtonSignCreate> 
+                <Text style={styles.signinText}>{signInCreateConstants.signinText}</Text>   
+                <InputText placeholder={signInCreateConstants.emailText} imagePath={emailIcon}></InputText>  
+                <InputText secureEntryValue={true} placeholder={signInCreateConstants.passwordText} imagePath={passwordIcon}></InputText> 
+                <Text style={styles.forgotText}>{signInCreateConstants.forgotPassword}</Text>  
+                <ButtonSignCreate ButtonText={signInCreateConstants.signinText}></ButtonSignCreate> 
             </View>
             
             <TouchableOpacity>
-                <Text style={styles.textOption}>CREATE ACCOUNT</Text>                                               
+                <Text style={styles.textOption}>{signInCreateConstants.createAccountText}</Text>                                               
             </TouchableOpacity>            
         </View>        
     );
