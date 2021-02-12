5ac74d2cb973871bf95dc26ee99b5e324feca279
Gerzain Magaña <47892616+vmagana3@users.noreply.github.com>
2021-02-12 11:07:04 -0600

    Update index.js

--- Vegezone/src/index.js
+++ Vegezone/src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import SignIn from './scenes/SignIn'
-import CreateAccount from './scenes/CreateAccount'
-import Home from './scenes/Home'
 
 const Index = () => {
-  return (    
-    //<SignIn></SignIn>          
-    //<CreateAccount></CreateAccount>
-    <Home></Home>
+  return (        
   );
 };
 
